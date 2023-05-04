e2820dacb573146b1fa4831b9f7b2fb45249b47c
VadBorovec <112804701+VadBorovec@users.noreply.github.com>
2023-05-04 18:29:45 +0300

    add styles

---
+++ src/js/constants/api.js
@@ -0,0 +1,8 @@
+export const API_KEY = 'f7c358c222822999296700cd3762c5cf';
+export const BASE_URL = 'https://api.themoviedb.org/3/';
+
+export const URL_TREND_DAY = 'trending/movie/day';
+export const URL_TREND_WEEK = 'trending/movie/week';
+
+// export const IMG_STANDARD_XLARGE = 'standard_xlarge';
+// export const IMG_NOT_AVAILABLE = 'image_not_available';
