24877ff31b5afb3c4942f75e5e6cc872d8e27cb4
VadBorovec <112804701+VadBorovec@users.noreply.github.com>
2023-05-05 18:48:31 +0300

    add styles for hero and change structure

--- src/js/components/Hero.js
+++ src/js/components/Hero.js
@@ -7,50 +7,46 @@ import { fetchTrendingMovies } from '../utils/fetchTrendDay';
 
 // import '../../sass/components/Hero.scss';
 
-const swiper = new Swiper('.swiper-container', {
-  slidesPerView: 3,
-  spaceBetween: 30,
-  loop: true,
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-});
+// const swiper = new Swiper('.swiper-container', {
+//   slidesPerView: 1,
+//   spaceBetween: 30,
+//   loop: true,
+//   pagination: {
+//     el: '.swiper-pagination',
+//     clickable: true,
+//   },
+// });
 
 // Function to create markup for a single movie
 function createMovieCardMarkup(movie) {
   const image = movie.poster_path
-    ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+    ? `https://image.tmdb.org/t/p/original${movie.backdrop_path}`
     : 'https://via.placeholder.com/500x750.png?text=No+Image';
   const title = movie.title || movie.name || 'Untitled';
   const releaseDate = movie.release_date || movie.first_air_date || 'N/A';
   const overview = movie.overview || 'No description available.';
   const rating = movie.rating || 'No rating';
-  const movieId = movie.movie_id;
-  const trailer = movie.videos;
+  // const movieId = movie.movie_id;
+  // const trailer = movie.videos;
 
   return `
     <div class="hero-trend__wrap">
-      <div class="hero-trend__details">
-        <h2 class="hero-trend__title">${title}</h2>
-        <p class="hero-trend__rating">${rating}</p>
-        <p class="hero-trend__overview">${overview}</p>
-        <button class="hero-trend__btn">Watch trailer</button>
-      </div>
-      <div class="hero-trend__image">
-      <img src="${image}" alt="${title}">
-      </div>
+      <div class="hero-trand__image" style="background-image: url('${image}')"></div>
+        <div class="hero-trend__info">
+          <div class="hero-trend__details" >
+            <h2 class="hero-trend__title">${title}</h2>
+            <p class="hero-trend__rating">${rating}</p>
+            <p class="hero-trend__overview">${overview}</p>
+            <button class="hero-trend__btn">Watch trailer</button>
+          </div>
+        </div>
+      
     </div>
   `;
 }
 
 // Function to render trending movies to the DOM
 async function renderTrendingMovies() {
-  // const ROOT_HERO_CONTAINER = document.querySelector('#trending-container');
   ROOT_HERO_CONTAINER.innerHTML = '';
 
   const swiperContainer = document.createElement('div');
@@ -63,7 +59,8 @@ async function renderTrendingMovies() {
     const trendingMovies = await fetchTrendingMovies();
 
     if (trendingMovies.length === 0) {
-      throw new Error('No data returned from the server.');
+      renderOnError();
+      // throw new Error('No data returned from the server.');
     }
 
     trendingMovies.forEach(movie => {
@@ -80,7 +77,20 @@ async function renderTrendingMovies() {
     ROOT_HERO_CONTAINER.appendChild(swiperContainer);
 
     const swiper = new Swiper('.swiper-container', {
-      // Add your Swiper options here
+      slidesPerView: 1,
+      spaceBetween: 0,
+      loop: true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        renderBullet: function (index, className) {
+          return '<span class="' + className + '">' + (index + 1) + '</span>';
+        },
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
     });
   } catch (error) {
     console.error(error);
@@ -89,7 +99,6 @@ async function renderTrendingMovies() {
 }
 
 export function renderOnError() {
-  // const ROOT_HERO_CONTAINER = document.getElementById('trending-container');
   ROOT_HERO_CONTAINER.innerHTML = `
     <div class="hero-trend__wrap">
       <img src="https://via.placeholder.com/500x750.png?text=No+Image" alt="Let's Make Your Own Cinema">
