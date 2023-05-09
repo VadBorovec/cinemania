accfc0708ece94faa5acea6866533b2ff5b63c69
VadBorovec <112804701+VadBorovec@users.noreply.github.com>
2023-05-09 03:38:18 +0300

    there are to much changes

--- src/js/components/Hero.js
+++ src/js/components/Hero.js
@@ -1,61 +1,87 @@
-// ! невірний шлях до файлу стилів
-// import Swiper, { Navigation, Pagination } from 'swiper';
-// import 'swiper/css';
-// import 'swiper/css/navigation';
-// import 'swiper/css/pagination';
-
-// import Swiper from 'swiper/bundle';
-// import Swiper, { Navigation, Pagination } from 'swiper';
-// import 'swiper/swiper-bundle.min.css';
-
-import { API_KEY, BASE_URL, URL_TREND_DAY } from '../constants/api';
+import { API_KEY, BASE_URL, IMG_URL } from '../constants/api';
 import { ROOT_HERO_CONTAINER } from '../constants/root';
-import { fetchTrendingMovies } from '../utils/fetchTrendDay';
-
-//! потрібно буде перемістити в constants/root.js
-export const IMG_URL = 'https://image.tmdb.org/t/p/original/';
+import { fetchTrendingMovies, getTrailer } from '../utils/fetchTrendDay';
+// import Swiper, { Navigation, Pagination } from 'swiper';
+// import 'swiper/swiper.min.css';
+// import 'swiper/modules/navigation/navigation.min.css';
+// import 'swiper/modules/pagination/pagination.min.css';
 
 // Function to create markup for a single movie
-function createMovieCardMarkup(movie) {
-  const image = movie.poster_path
+async function createMovieCardMarkup(movie) {
+  const image = movie.backdrop_path
     ? `${IMG_URL}${movie.backdrop_path}`
-    : 'https://via.placeholder.com/500x750.png?text=No+Image';
+    : `${IMG_URL}${movie.poster_path}`;
   const title = movie.title || movie.name || 'Untitled';
-  // const releaseDate = movie.release_date || movie.first_air_date || 'N/A';
   const overview = movie.overview || 'No description available.';
   const rating = movie.vote_average || 'No rating';
   const id = movie.id;
-  console.log('id:', id);
-  const video = movie.video;
-  console.log('video:', video);
-  console.log('len', overview.length);
 
-  // ! невірний шлях .. знайти в документації
-  const trailer = movie.videos;
+  try {
+    const trailer = await getTrailer(id);
+    const trailerLink = trailer
+      ? `https://www.youtube.com/watch?v=${trailer}`
+      : 'openModal()';
 
-  return `
-    <div class="hero-trend__wrap swiper-slide">
-      <div class="hero-trand__image" style="background-image: url('${image}')"></div>
-        <div class="hero-trend__info">
-          <div class="hero-trend__details" >
+    return `
+      <div class="hero-trend__wrap swiper-slide">
+        <div class="hero-trend__bgd" style="background-image: url('${image}')"></div>
+        <div class="hero-trend__info hero-trend__info-light container">
+          <div class="hero-trend__details hero-trend__details-light">
             <h2 class="hero-trend__title">${
-              title.length > 30 ? title.substring(0, 30) + '...' : title
+              title.length > 25 ? title.substring(0, 25) + '...' : title
             }</h2>
-            <p class="hero-trend__rating">${
-              Math.round(rating * 10) / 10
-            } / 10</p>
-            <p class="hero-trend__overview">${
-              overview.length > 245
-                ? overview.substring(0, 245) + '...'
+            <div class="hero-trend__rating ${stars(
+              Number(rating.toFixed(1))
+            )}"></div>
+            <p class="hero-trend__overview hero-trend__overview-light">${
+              overview.length > 300
+                ? overview.substring(0, 300) + '...'
                 : overview
             }</p>
-            <button class="hero-trend__btn">
-              <a href="${BASE_URL}movie/${id}/${video}?api_key=${API_KEY}" class="hero-trend__btn-link">Watch trailer</a>
+            <button id="modal-trigger" type="button" class="hero-trend__btn" onclick="${
+              trailerLink
+                ? `window.open('${trailerLink}', '_blank')`
+                : 'openModal()'
+            }">
+              Watch trailer
             </button>
           </div>
         </div>
-    </div>
-  `;
+      </div>
+    `;
+  } catch (error) {
+    console.error(error);
+    defaultHeroMarkup();
+  }
+}
+
+// to render Rating in stars
+function stars(vote) {
+  if (vote === 10) {
+    return 'ten-stars';
+  } else if (vote < 10 && vote > 8) {
+    return 'nine-stars';
+  } else if (vote === 8) {
+    return 'eight-stars';
+  } else if (vote < 8 && vote > 6) {
+    return 'seven-stars';
+  } else if (vote === 6) {
+    return 'six-stars';
+  } else if (vote < 6 && vote > 4) {
+    return 'five-stars';
+  } else if (vote === 4) {
+    return 'four-stars';
+  } else if (vote < 4 && vote > 2) {
+    return 'three-stars';
+  } else if (vote === 2) {
+    return 'two-stars';
+  } else if (vote < 2 && vote > 0) {
+    return 'one-star';
+  } else if (vote === 0) {
+    return 'zero-star';
+  } else if (!vote) {
+    return 'No rating';
+  }
 }
 
 // Function to render trending movies to the DOM
@@ -67,14 +93,13 @@ async function renderTrendingMovies() {
     const trendingMovies = await fetchTrendingMovies();
 
     if (trendingMovies.length === 0) {
-      renderOnError();
+      defaultHeroMarkup();
     }
 
-    trendingMovies.forEach(movie => {
-      const movieCardMarkup = createMovieCardMarkup(movie);
-
+    for (let i = 0; i < trendingMovies.length; i++) {
+      const movieCardMarkup = await createMovieCardMarkup(trendingMovies[i]);
       markup += movieCardMarkup;
-    });
+    }
 
     swiperWrap.innerHTML = markup;
 
@@ -116,7 +141,7 @@ async function renderTrendingMovies() {
 export function defaultHeroMarkup() {
   ROOT_HERO_CONTAINER.innerHTML = `
       <div class="hero-trend__wrap">
-      <div class="hero-trand__image hero-trand__image-back"></div>
+      <div class="hero-trend__bgd hero-trend__bgd-default"></div>
         <div class="hero-trend__info">
           <div class="hero-trend__details" >
             <h2 class="hero-trend__title">Let's Make Your Own Cinema</h2>
@@ -132,3 +157,42 @@ export function defaultHeroMarkup() {
 
 // Call renderTrendingMovies to initially render trending movies
 renderTrendingMovies();
+
+//! IDN doesn't work - openLinkInNewTab(url)
+// function openLinkInNewTab(url) {
+//   window.open(url, '_blank');
+// }
+
+// ! to open modal
+function openModal() {
+  const modal = document.getElementById('modal');
+  modal.style.display = 'block';
+}
+
+// Get the Modal Overlay and Modal Content elements
+const modalOverlay = document.getElementById('modal-overlay');
+const modal = document.getElementById('modal');
+
+// Get the Modal Trigger Button and Modal Close Button elements
+const modalTrigger = document.getElementById('modal-trigger');
+const modalClose = document.getElementById('modal-close');
+
+// Add a click event listener to the Modal Trigger Button
+modalTrigger.addEventListener('click', () => {
+  // Show the Modal Overlay
+  modalOverlay.style.display = 'block';
+});
+
+// Add a click event listener to the Modal Close Button
+modalClose.addEventListener('click', () => {
+  // Hide the Modal Overlay
+  modalOverlay.style.display = 'none';
+});
+
+// Add a click event listener to the Modal Overlay (to close the modal if clicked outside of it)
+modalOverlay.addEventListener('click', event => {
+  if (event.target === modalOverlay) {
+    // Hide the Modal Overlay
+    modalOverlay.style.display = 'none';
+  }
+});
--- src/js/utils/fetchTrendDay.js
+++ src/js/utils/fetchTrendDay.js
@@ -8,8 +8,26 @@ export async function fetchTrendingMovies() {
     const response = await axios.get(
       `${BASE_URL}${URL_TREND_DAY}?api_key=${API_KEY}`
     );
+
     const trendingMovies = response.data.results.slice(0, 5);
+
     return trendingMovies;
+  } catch (error) {
+    console.error(error);
+    defaultHeroMarkup();
+  }
+}
+
+// Function to fetch movie trailer
+export async function getTrailer(movie_id) {
+  try {
+    const response = await axios.get(
+      `${BASE_URL}movie/${movie_id}/videos?api_key=${API_KEY}`
+    );
+
+    const movieTrailer = await response.data.results[0].key;
+
+    return movieTrailer;
   } catch (error) {
     console.error(error);
     defaultHeroMarkup();
